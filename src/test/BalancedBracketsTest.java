2e3c9ac0a8770a33ea28d8c12157fa741703014f
Kelsey Niederer <kelseyniederer12@gmail.com>
2020-05-11 13:38:32 -0500

    added 12 unit tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,77 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
+    // Test 1
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void emptyBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    // Test 2
+    @Test
+    public void balancedBracketsWithWords(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    // Test 3
+    @Test
+    public void balancedBracketsWithinString(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    // Test 4
+    @Test
+    public void stringWithOddBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch]Code]"));
+    }
+
+    // Test 5
+    @Test
+    public void reverseBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    // Test 6
+    @Test
+    public void squareBracketsWithOtherBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]}("));
+    }
+
+    // Test 7
+    @Test
+    public void closedCurlyBracketsOnlyReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{[}"));
+    }
+
+    //Test 8
+    @Test
+    public void multipleClosedBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+
+    // Test 9
+    @Test
+    public void oneClosedBracketOneOpen(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    // Test 10
+    @Test
+    public void oneClosedOneOpenWithChars(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Hey]["));
+    }
+
+    // Test 11
+    @Test
+    public void multipleBracketsWithCharsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Kelsey][Lee]"));
+    }
+
+    // Test 12
+    @Test
+    public void oneClosedSetOneReverseSet(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][]["));
+    }
 
 }
